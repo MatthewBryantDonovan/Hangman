30c41e5fbdd01d48512c8459eb3ac76a9aa8eb22
Matthew Donovan <matthewbryantdonovan@gmail.com>
2019-10-24 11:17:52 -0400

    made it responsive on mobilegit add -Agit add -A yesgit add -Agit add -A!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,43 +28,27 @@ var winSevenMP3 = document.getElementById("win-seven");
 var winEightMP3 = document.getElementById("win-eight");
 var winNineMP3 = document.getElementById("win-nine");
 var winTenMP3 = document.getElementById("win-ten");
-/* var x = window.matchMedia("(max-width: 535px)") */
+var x = window.matchMedia("(max-width: 1025px)")
 
 
-
-
-// the code commented below is an attempt to make the virtual keyboard open on mobile
-// var mq = window.matchMedia('@media all and (max-width: 700px)');
-/* function vKB() {
-  $("#mobileKB").focus();
-  console.log("11112222");
-}
-
-var target = document.getElementsByTagName("mobileKB")[0];
-
-if (target != target) {
-    target.focus();
-    target.click();
-    console.log("4445555");
-} */
-/*   document.getElementById("mobileKB").click = console.log("clicked"); */
-  //make a div with a high z-index to  that tells user to move phone sideways
-/* if (x.matches) {
+  //TODO: make a div with a high z-index to  that tells user to move phone sideways
+if (x.matches) {
   console.log("small screen");
-
+  document.getElementById("vkb-toggle").style.display = "initial";
 } else {
   console.log("big screen");
+  document.getElementById("vkb-toggle").style.display = "none";
 }
 
 x.addListener(function (changed) {
   if (changed.matches) {
     console.log("small screen");
+    document.getElementById("vkb-toggle").style.display = "initial";
   } else {
     console.log("big screen");
 
   }
-}); */
-    //prompt user so the keypad pops up
+});
 
 function mainSound() {
   mainThemeMP3.volume = 0.2;
@@ -192,7 +176,9 @@ document.onkeyup = function (getkey) {
       document.getElementById("user-guessed").innerHTML = "Letters already guessed: ";
       firstInput = true;
     } else {
+
       userGuess = getkey.key;
+      userGuess = Keyboard.lastKeyPressed;
       userGuess = userGuess.toLowerCase();
       document.getElementById("error").innerHTML = "";
       //only run logic if user inputs a letter
@@ -296,7 +282,6 @@ document.onkeyup = function (getkey) {
           gameStart = true;
         }
       } else {
-        console.log(userGuess.search(/[^a-zA-Z]+/));
       }
     }
   } else {
@@ -306,4 +291,172 @@ document.onkeyup = function (getkey) {
   }
   mainSound();
 }
-// new game word never equals old game word
+
+
+
+
+// try to find a way to make the mobile version to work in the onkeyup
+
+document.onclick = function (getkey) {
+  if (wins < wordPool.length) {
+
+    //establish new game info
+    if (gameStart == true) {
+      if (hasWon == true) {
+        pastGameWords.push(currentWord);
+        hasWon = false;
+      }
+      if (wordPool.length != pastGameWords.length) {
+        do {
+          currentWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+        } while (pastGameWords.indexOf(currentWord) != -1);
+      }
+      currentWordAr = currentWord.toLowerCase().split("");
+      currentVsUser = [];
+      userGuessed = [];
+      document.getElementById("user-guessed").innerHTML = "Letters already guessed: " + userGuessed.join(" ");
+      amountCorrect = 0;
+      guessesRemaining = 6;
+      //make an array of blank spaces for user to guess
+      for (var i = 0; i < currentWord.length; i++) {
+        currentVsUser.push("_");
+      }
+
+      //make the win condition equal to the length of the word we are guessing
+      winCon = currentWord.length;
+
+      //reset info for user
+      document.getElementById("current-word").innerHTML = "Facility password: " + currentVsUser.join(" ");
+      document.getElementById("guesses-Remaining").innerHTML = "Incorrect guesses remaining: " + guessesRemaining;
+      gameStart = false;
+      firstInput = false;
+    }
+    // end gamestart
+
+
+
+
+    if (firstInput == false) {
+      document.getElementById("start").innerHTML = "";
+      document.getElementById("play-again").innerHTML = "";
+      document.getElementById("current-word").innerHTML = "Facility password: " + currentVsUser.join(" ");
+      document.getElementById("wins-").innerHTML = "Successful hacks: " + wins;
+      document.getElementById("guesses-Remaining").innerHTML = "Incorrect guesses remaining: " + guessesRemaining;
+      document.getElementById("user-guessed").innerHTML = "Letters already guessed: ";
+      firstInput = true;
+    } else {
+      
+      userGuess = Keyboard.lastKeyPressed;
+      userGuess = userGuess.toLowerCase();
+      
+      document.getElementById("error").innerHTML = "";
+      //only run logic if user inputs a letter
+      if (userGuess.search(/[a-z]/) === 0 && userGuess.length == 1) {
+
+        //see if user guessed the letter before
+        for (var m = 0; m < userGuessed.length; m++) {
+          if (userGuessed[m] == userGuess) {
+            guessedBefore = true;
+          }
+        }
+
+        //add the letters to the board if found in word
+        var a = 0;
+        if (guessedBefore == false) {
+          userGuessed.push(userGuess);
+          currentWordAr.forEach(function (currentLetter) {
+            if (userGuess === currentWordAr[a]) {
+              currentVsUser[a] = currentLetter;
+              guessedCorrectly = true;
+              //add the amount of letters correct to check against win conditon later
+              amountCorrect++;
+            }
+            a++
+          });
+        } else {
+          document.getElementById("error").innerHTML = "! Snake, you tried that already !";
+          alertSound();
+          document.getElementById("error").style.color = "red";
+          /*           document.getElementById("error").style.display = "none"; */
+        }
+
+        // if used didn't guess correctly - take away a guess
+        if (guessedCorrectly == false && guessedBefore == false) {
+          guessesRemaining--;
+        }
+
+        // reset and tally varibles for next run
+        guessedCorrectly = false;
+        guessedBefore = false;
+        document.getElementById("user-guessed").innerHTML = "Letters already guessed: " + userGuessed.join(" ");
+        document.getElementById("current-word").innerHTML = "Facility password: " + currentVsUser.join(" ");
+        document.getElementById("guesses-Remaining").innerHTML = "Incorrect guesses remaining: " + guessesRemaining;
+
+        //winning condition
+        if (winCon == amountCorrect) {
+          wins++;
+          document.getElementById("current-word").innerHTML = "";
+          document.getElementById("wins-").innerHTML = "";
+          document.getElementById("guesses-Remaining").innerHTML = "";
+          document.getElementById("user-guessed").innerHTML = "";
+
+          if (wins == 1) {
+            document.getElementById("wins-").innerHTML = "Password entered: " + currentWord;
+            winOneSound();
+            document.getElementById("play-again").innerHTML = "That's one password down. Keep 'em coming snake!";
+          } else if (wins < wordPool.length) {
+            document.getElementById("wins-").innerHTML = "Password entered: " + currentWord;
+            if (wins == 2) {
+              winTwoSound();
+            }
+            if (wins == 3) {
+              winThreeSound();
+            }
+            if (wins == 4) {
+              winFourSound();
+            }
+            if (wins == 5) {
+              winFiveSound();
+            }
+            if (wins == 6) {
+              winSixSound();
+            }
+            if (wins == 7) {
+              winSevenSound();
+            }
+            if (wins == 8) {
+              winEightSound();
+            }
+            if (wins == 9) {
+              winNineSound();
+            }
+            document.getElementById("play-again").innerHTML = "That's another password down! One step closer to entry!";
+          } else if (wins == wordPool.length) {
+            document.getElementById("wins-").innerHTML = "Password entered: " + currentWord;
+            winTenSound();
+            document.getElementById("play-again").innerHTML = "You hacked all the passwords!";
+
+          }
+          gameStart = true;
+          hasWon = true;
+        }
+        //losing condition
+        if (guessesRemaining === 0) {
+          document.getElementById("current-word").innerHTML = "";
+          document.getElementById("wins-").innerHTML = "";
+          document.getElementById("guesses-Remaining").innerHTML = "";
+          document.getElementById("user-guessed").innerHTML = "";
+          document.getElementById("play-again").innerHTML = "Snake? What Happened!? Snake!? Snaaaake!";
+          snakeSound();
+          gameStart = true;
+        }
+      } else {
+      }
+    }
+  } else {
+    document.getElementById("wins-").innerHTML = "";
+    document.getElementById("mission-complete").innerHTML = "Quick infiltrate the facility!";
+    document.getElementById("play-again").innerHTML = "";
+  }
+  mainSound();
+}
